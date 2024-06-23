77bbc222f6c4dd826c4a4b4a297a10a552e57a97
Ashutosh singh <138657449+AshutoshSingh00001@users.noreply.github.com>
2024-06-23 12:13:21 +0530

    feat: description in dialog

--- vite-project/src/Polkadot Asset Hub/item.jsx
+++ vite-project/src/Polkadot Asset Hub/item.jsx
@@ -2708,6 +2708,24 @@ const ipfsItemUri = `ipfs://${ipfsItemHash}`;
         <CardBody>
         <div className="flex items-center">
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+  </svg>
+
+  <Typography color="blue-gray" className="font-medium text-3xl">
+    Description
+  </Typography>
+</div>
+
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Typography variant="h6">{itemMetadata.description}</Typography>
+</CardFooter>
+</Card>
+
+<Card className="mt-20 w-full   ">
+        <CardBody>
+        <div className="flex items-center">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
   </svg>
